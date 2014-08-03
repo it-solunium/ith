8061dbedfaede37523614602876e3b97d83bea33
IT SOLUNIUM <it.solunium@gmail.com>
2014-08-03 19:23:10 +0100

    Create config.py
    Configuration file

---
+++ config.py
@@ -0,0 +1,16 @@
+#!/usr/bin/env python
+# -*- coding: UTF-8 -*-
+
+
+#Database configuration
+
+class bddConfiguration:
+    
+    host = "localhost"
+    user = "root"
+    passwd = "YourPassword"
+    db = "google_dorks"
+    
+#User configuration
+    lang = "EN"
+
